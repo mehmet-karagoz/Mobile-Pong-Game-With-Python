7b6e888c332bd6f64a3f9036ed23c94431d6c95a
mehmet-karagoz <atleownhem@gmail.com>
2021-08-17 20:30:57 +0300

    added pong paddles

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from random import randint
-
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.properties import NumericProperty, ReferenceListProperty, \
@@ -10,9 +8,15 @@ from kivy.vector import Vector
 
 class PongGame(Widget):
     ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
 
     def update(self, dt):
         self.ball.move()
+
+        # bounce of paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
 
         # set the limits to which the ball can go
         # for y coordinate
@@ -22,9 +26,23 @@ class PongGame(Widget):
         if self.ball.x < 0 or self.ball.right > self.width:
             self.ball.velocity_x *= -1
 
-    def serve_ball(self):
+        # score
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel=(3, 0))
+        if self.ball.x > self.width - 20:
+            self.player1.score += 1
+            self.serve_ball(vel=(-3, 0))
+
+    def serve_ball(self, vel=(3, 0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(3, 0).rotate(randint(0, 90))
+        self.ball.velocity = vel
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width / 3:
+            self.player2.center_y = touch.y
 
 
 class Ball(Widget):
@@ -34,7 +52,19 @@ class Ball(Widget):
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+        self.pos = Vector(self.velocity) + self.pos
+
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            vx *= -1.1
+            vy *= 1.1
+            ball.velocity = vx, vy + offset
 
 
 class PongApp(App):
